d12d8a6b73535bb1d7cc8303e735f0ceb21e38cf
Tim <tim.maastricht@gmail.com>
2025-10-31 17:14:32 +1000

    fixed auto schema

--- app.py
+++ app.py
@@ -26,6 +26,22 @@ class Card(db.Model):
     status = db.Column(db.String())
     priority = db.Column(db.String())
     
+    
+#create the Card Schema with Marshmallow, 
+#it will provide the serialization needed for converting the data into JSON
+class CardSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        # Fields to expose
+        model = Card
+        load_instance = True
+
+
+#single card schema, when one card needs to be retrieved
+card_schema = CardSchema()
+
+#multiple card schema, when many cards need to be retrieved
+cards_schema = CardSchema(many=True)
+    
 # create app's cli command named create, then run it in the terminal as "flask create", 
 # it will invoke create_db function
 @app.cli.command("create")
@@ -68,6 +84,7 @@ def seed_db():
 def drop_db():
     db.drop_all()
     print("Tables dropped") 
+  
 
 @app.route("/")
 def hello():
@@ -77,6 +94,9 @@ def hello():
 def get_cards():
     #get all the cards from the database table
     stmt = db.select(Card)
-    cards = db.session.scalars(stmt)
-    return cards
+    cards = db.session.scalars(stmt).all()
+    # Convert the cards from the database into a JSON format and store them in result
+    result = cards_schema.dump(cards)
+    #return result in JSON format
+    return jsonify(result)
 
